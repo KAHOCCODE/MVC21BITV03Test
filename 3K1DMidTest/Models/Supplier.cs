9a2b355533605ec418e5ffb355c8437064788d9a
ka do doka <hoangkavudo@gmail.com>
2024-08-09 13:35:46 +0700

    Add project files.

---
+++ 3K1DMidTest/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3K1DMidTest.Models;
+
+public partial class Supplier
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool IsImporter { get; set; }
+
+    public virtual ICollection<Part> Parts { get; set; } = new List<Part>();
+}
